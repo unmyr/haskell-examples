9b41f2c7c0229ca931b9cd489b1c7a9624e8b8c1
unmyr <56982541+unmyr@users.noreply.github.com>
2021-12-18 03:12:35 +0900

    refactor(prelude): Improve an example of Higher-order function.

--- src/prelude/higher_order_function.hs
+++ src/prelude/higher_order_function.hs
@@ -1,10 +1,8 @@
 compute :: ((Double, Double) -> Double) -> Double
 compute (f) = f (3::Double, 4::Double)
 
-hypot :: (Double, Double) -> Double
-hypot (x, y) = sqrt (x^2 + y^2)
-
 main = do
+    let hypot = \(x, y) -> sqrt (x^2 + y^2)
     print $ hypot(5, 12)
     print $ compute(hypot)
     print $ compute((\(x, y) -> x**y))
