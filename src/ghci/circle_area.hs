5fd86db12b269315f522a10ab7c9ecdf67bf48f4
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-17 23:21:00 +0900

    feat: Add an example of Floating type class.

---
+++ src/ghci/circle_area.hs
@@ -0,0 +1,8 @@
+circle_area_float :: Float -> Float
+circle_area_float r = pi * r * r
+
+circle_area_double :: Double -> Double
+circle_area_double r = pi * r * r
+
+circle_area_floating :: Floating a =>  a -> a
+circle_area_floating r = pi * r * r
