9aa59671e0ebe3a5739a791e3edb29048310dc4d
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-17 23:01:08 +0900

    feat(composite-types): Improve examples.

--- src/ghci/composite-types/vertex_named_fields.hs
+++ src/ghci/composite-types/vertex_named_fields.hs
@@ -1,12 +1,23 @@
-data Vertex = Vertex { x :: Int, y :: Int }
+data Vertex = Vertex { vertex_x :: Int, vertex_y :: Int }
 instance Show Vertex where
-  show (Vertex x y) = "Vertex (" ++ show x ++ "," ++ show y ++ ")"
+  show (Vertex x y) = "Vertex {x=" ++ show x ++ ",y=" ++ show y ++ "}"
+
+class DistanceSpace a where
+  distance :: (a, a) -> Float
+
+instance DistanceSpace Vertex where
+  distance (v1, v2) =
+    sqrt(fromIntegral ((vertex_x v2 - vertex_x v1)^2 + (vertex_y v2 - vertex_y v1)^2))
+
+global_scoped_distance :: (Vertex, Vertex) -> Float
+global_scoped_distance (v1, v2) =
+    sqrt(fromIntegral ((vertex_x v2 - vertex_x v1)^2 + (vertex_y v2 - vertex_y v1)^2))
 
 main = do
   let a = Vertex 3 4
       b = Vertex 2 3
   print a
-  print $ x a
-  print (y a)
-  print $ (\v -> sqrt(fromIntegral (x v * x v + y v * y v))) a
+  print $ vertex_x a
+  print (vertex_y a)
+  print $ (\v -> sqrt(fromIntegral (vertex_x v ^2 + vertex_y v ^2))) a
   print b
--- src/ghci/composite-types/vertex_no_named_fields.hs
+++ src/ghci/composite-types/vertex_no_named_fields.hs
@@ -1,5 +1,20 @@
 data Vertex = Vertex Int Int deriving (Show)
 
+class DistanceSpace a where
+  distance :: (a, a) -> Float
+
+instance DistanceSpace Vertex where
+  distance (v1, v2) =
+    sqrt(fromIntegral(
+      ((\(Vertex x _) -> x) v2 - (\(Vertex x _) -> x) v1)^2 +
+      ((\(Vertex _ y) -> y) v2 - (\(Vertex _ y) -> y) v1)^2))
+
+global_scoped_distance :: (Vertex, Vertex) -> Float
+global_scoped_distance (v1, v2) =
+  sqrt(fromIntegral(
+    ((\(Vertex x _) -> x) v2 - (\(Vertex x _) -> x) v1)^2 +
+    ((\(Vertex _ y) -> y) v2 - (\(Vertex _ y) -> y) v1)^2))
+
 main = do
   let a = Vertex 3 4
       b = Vertex 2 3
