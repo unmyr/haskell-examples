548323ecc441d5c8b7df6c6a593a6a032b07b307
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-24 04:02:02 +0900

    refactor(math): Using `Num`.

--- src/math/square_root_int.hs
+++ src/math/square_root_int.hs
@@ -1,6 +1,8 @@
-squareRoot :: Int -> Int
+-- squareRoot :: Int -> Int
+squareRoot :: (Eq a, Ord a, Num a, Integral a) => a -> a
 squareRoot n | n == 0 = 0
 squareRoot n | n == 1 = 1
+squareRoot n | n < 0  = error "ERROR: number must be positive."
 squareRoot n = squareRootHelper n (n `div` 2 + 1)
   where 
   squareRootHelper n_ k
@@ -8,7 +10,8 @@ squareRoot n = squareRootHelper n (n `div` 2 + 1)
     | 1 < k && n_  < k*k = squareRootHelper n_ (k-1)
     | otherwise          = error "ERROR: Square root not found."
 
-squareRootM :: Int -> Maybe Int
+-- squareRootM :: Int -> Maybe Int
+squareRootM :: (Eq a, Ord a, Num a, Integral a) => a -> Maybe a
 squareRootM n | n<0 = Nothing
 squareRootM n = do
   let match_list = [ x | x <- [2..(n `div` 2) + 1], x^(2 :: Int) == n]
