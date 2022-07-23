0a7e607eebe93e81bd86b192dc55208ccc5807f8
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-23 18:01:41 +0900

    fix(math/group): Fix Q8 multiplication table.

--- src/math/group/group_o8_q8_nc_debug.hs
+++ src/math/group/group_o8_q8_nc_debug.hs
@@ -1,6 +1,7 @@
 import Debug.Trace
 
 q8_e = "e"
+q8_all = [q8_e, "s", "i", "si", "j", "sj", "k", "sk"]
 
 q8_dot x y | x == q8_e = y
 q8_dot x y | y == q8_e = x
@@ -41,7 +42,7 @@ q8_dot x y | x == "j" && y == "sk" = "si"
 q8_dot x y | x == "sj" && y == "s" = "j"
 q8_dot x y | x == "sj" && y == "i" = "k"
 q8_dot x y | x == "sj" && y == "si" = "sk"
-q8_dot x y | x == "sj" && y == "j" = "sk"
+q8_dot x y | x == "sj" && y == "j" = q8_e
 q8_dot x y | x == "sj" && y == "sj" = "s"
 q8_dot x y | x == "sj" && y == "k" = "si"
 q8_dot x y | x == "sj" && y == "sk" = "i"
@@ -61,7 +62,6 @@ q8_dot x y | x == "sk" && y == "sj" = "si"
 q8_dot x y | x == "sk" && y == "k" = q8_e
 q8_dot x y | x == "sk" && y == "sk" = "s"
 
-
 q8_dot x y = trace ("DEBUG: x=" ++ show x ++ ", y=" ++ show y) "?"
 
 q8 :: [String] -> [String]
@@ -72,5 +72,16 @@ q8 (x:xs) = trace ("DEBUG1+: x=" ++ show x ++ ", xs=" ++ show xs ++ ", len=" ++ 
 q8_inv x = head [x | y <- [q8_e, "s", "i", "si", "j", "sj", "k", "sk"], q8_dot x y == q8_e]
 
 main = do
-  putStrLn $ "i * j * k = " ++ head (q8 ["si", "i", "j", "k"])
+  print $ [q8_dot "e"  y | y <- q8_all] == [ "e", "s", "i","si", "j","sj", "k","sk"]
+  print $ [q8_dot "s"  y | y <- q8_all] == [ "s", "e","si", "i","sj", "j","sk", "k"]
+  print $ [q8_dot "i"  y | y <- q8_all] == [ "i","si", "s", "e", "k","sk","sj", "j"]
+  print $ [q8_dot "si" y | y <- q8_all] == ["si", "i", "e", "s","sk", "k", "j","sj"]
+  print $ [q8_dot "j"  y | y <- q8_all] == [ "j","sj","sk", "k", "s", "e", "i","si"]
+  print $ [q8_dot "sj" y | y <- q8_all] == ["sj", "j", "k","sk", "e", "s","si", "i"]
+  print $ [q8_dot "k"  y | y <- q8_all] == [ "k","sk", "j","sj","si", "i", "s", "e"]
+  print $ [q8_dot "sk" y | y <- q8_all] == ["sk", "k","sj", "j", "i","si", "e", "s"]
+  putStrLn $ "si * si = " ++ head (q8 ["si", "si"])
+  putStrLn $ " i * sk = " ++ head (q8 ["i", "sj"])
+  putStrLn $ "si * si  = " ++ head (q8 ["si", "si"])
+  putStrLn $ "si * i * j * k = " ++ head (q8 ["si", "i", "j", "k"])
   putStrLn $ "(si)^(-1) = " ++ q8_inv "si"
