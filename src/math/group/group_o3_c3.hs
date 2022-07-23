82fc81f007fa4fbf00a3b5a68db3c52ad07e742c
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-23 18:40:57 +0900

    docs: Add articles related to group theory.

--- src/math/group/group_o3_c3.hs
+++ src/math/group/group_o3_c3.hs
@@ -1,6 +1,8 @@
 c3_e = 0
+c3_all = [0, 1, 2]
+
 c3_op x y = (x + y) `mod` 3
-c3_inv x = head [y | y <- [0, 1, 2], c3_op x y == c3_e]
+c3_inv x = head [y | y <- c3_all, c3_op x y == c3_e]
 
 c3 :: [Int] -> [Int]
 c3 [] = []
@@ -8,6 +10,9 @@ c3 [x] = [x]
 c3 (x:xs) = c3 ([c3_op x (head xs)] ++ (tail xs))
 
 main = do
+  print $ [c3_op 0  y | y <- c3_all]
+  print $ [c3_op 1  y | y <- c3_all]
+  print $ [c3_op 2  y | y <- c3_all]
   putStrLn $ "0^(-1) = " ++ show (c3_inv 0)
   putStrLn $ "1^(-1) = " ++ show (c3_inv 1)
   putStrLn $ "1 * 2 = " ++ show (head (c3 [1, 2]))
