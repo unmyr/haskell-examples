e5bb2f7dba42cd0873d8d8562bc644094262bbcc
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-24 13:37:40 +0900

    fix(functions): Fix build failed.

--- src/functions/circle_area.hs
+++ src/functions/circle_area.hs
@@ -4,5 +4,12 @@ circle_area_float r = pi * r * r
 circle_area_double :: Double -> Double
 circle_area_double r = pi * r * r
 
-circle_area_floating :: Floating a =>  a -> a
+circle_area_floating :: Floating a => a -> a
 circle_area_floating r = pi * r * r
+
+main :: IO ()
+main = do
+    print $ circle_area_float 1
+    print $ circle_area_double 1
+    print $ circle_area_floating (1 :: Float)
+    print $ circle_area_floating (1 :: Double)
