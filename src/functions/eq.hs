4f4d719abf94876f89187fc6262cd1879b5f272f
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-24 18:55:07 +0900

    feat(functions): Add an example of `Eq`.

---
+++ src/functions/eq.hs
@@ -0,0 +1,11 @@
+eq :: (Ord a) => a -> a -> Bool
+eq x y = if x == y then True else False
+
+main :: IO ()
+main = do
+  print $ eq (3 :: Int) 3
+  print $ eq (1/3 :: Rational) (1/3 :: Rational)
+  print $ eq (pi::Float) 3.1415927
+  print $ eq (pi::Double) 3.141592653589793
+  print $ eq 'a' 'a'
+  print $ eq "hello" "hello"
