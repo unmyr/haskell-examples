36e5fee94e93fcf3f599c763c20225b1376a1a20
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-24 13:10:54 +0900

    refactor(functions): Improve test codes.

--- src/functions/add_2arg_int.hs
+++ src/functions/add_2arg_int.hs
@@ -3,7 +3,7 @@ add x y = x + y
 
 main :: IO ()
 main = do
-    print(add 42 13)
+    print $ add 42 13 == 55
     print $ add (maxBound :: Int) 0 == 9223372036854775807
     print $ add (maxBound :: Int) 1 == (minBound :: Int)
     print $ add (minBound :: Int) (-1) == (maxBound :: Int)
