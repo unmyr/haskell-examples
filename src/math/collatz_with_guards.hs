c7204af9f6c6b18340b328660c7d1735a8e8c6eb
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-27 01:47:04 +0900

    refactor(math): Use colon(`:`) operator instead `[]`.

--- src/math/collatz_with_guards.hs
+++ src/math/collatz_with_guards.hs
@@ -1,7 +1,7 @@
 collatz :: Integer -> [Integer]
 collatz n | n == 1         = [1]
-          | n `mod` 2 == 0 = [n] ++ collatz (n `div` 2)
-          | otherwise      = [n] ++ collatz (n * 3 + 1)
+          | n `mod` 2 == 0 = n : collatz (n `div` 2)
+          | otherwise      = n : collatz (n * 3 + 1)
 
 main :: IO ()
 main = do
--- src/math/collatz_with_pattern.hs
+++ src/math/collatz_with_pattern.hs
@@ -1,7 +1,7 @@
 collatz :: Integer -> [Integer]
 collatz 1 = [1]
-collatz n | even n    = [n] ++ collatz (n `div` 2)
-          | otherwise = [n] ++ collatz (n * 3 + 1)
+collatz n | even n    = n : collatz (n `div` 2)
+          | otherwise = n : collatz (n * 3 + 1)
 
 main :: IO ()
 main = do
