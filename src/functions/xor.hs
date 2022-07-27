38d7cf7375c420978cba223529806f3bfe661161
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-27 21:43:54 +0900

    refactor(functions): Refactor the xor code example.

--- src/functions/xor.hs
+++ src/functions/xor.hs
@@ -2,12 +2,17 @@ import Debug.Trace
 
 xor :: (Integral a, Show a) => [a] -> [a]
 xor [] = trace("DEBUG[]") []
-xor [x] = trace("DEBUG[" ++ show x ++ "]") [x]
+xor [x] = trace("DEBUG[" ++ show x ++ "]") $ x:[]
+xor [x, y] =
+  trace("DEBUG[" ++ show x ++ "," ++ show y ++ "]") $
+  ((x + y) `mod` 2):[]
 xor (x:y:zs) =
-  trace ("DEBUG(" ++ show x ++ ":" ++ show y ++ ":" ++ show zs ++ ")")
-  xor ([(x + y `mod` 2) `mod` 2] ++ zs)
+  trace ("DEBUG(" ++ show x ++ ":" ++ show y ++ ":" ++ show zs ++ ")") $
+  xor (xor [x, y] ++ zs)
 
 main :: IO ()
 main = do
   print $ xor ([] :: [Int]) == []
+  print $ xor ([1, 1] :: [Int]) == [0]
+  print $ xor ([1, 1, 1] :: [Int]) == [1]
   print $ xor ([1, 1, 1, 1] :: [Int]) == [0]
