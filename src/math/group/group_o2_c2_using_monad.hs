c73085ca9ceb89b73496c6b5d90c46c4886970bf
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-28 02:52:34 +0900

    refactor(math/group): Using `mconcat`.

--- src/math/group/group_o2_c2_using_monad.hs
+++ src/math/group/group_o2_c2_using_monad.hs
@@ -16,15 +16,9 @@ instance Monoid C2 where
 c2_inv :: C2 -> C2
 c2_inv (C2 x) = head [C2 y | y <- [0..2], (C2 x) `mappend` (C2 y) == (mempty)]
 
-c2_eval :: [C2] -> [C2]
-c2_eval [] = []
-c2_eval [x] = [x]
-c2_eval [x, y] = (x `mappend` y):[]
-c2_eval (x:y:zs) = c2_eval ((c2_eval [x, y]) ++ zs)
-
 main :: IO ()
 main = do
   putStrLn $ "0^(-1) = " ++ show (c2_inv (C2 0))
   putStrLn $ "1^(-1) = " ++ show (c2_inv (C2 1))
-  putStrLn $ "1 ⊕ 1 ⊕ 1 = " ++ show (head (c2_eval [(C2 1), (C2 1), (C2 1)]))
-  putStrLn $ "1 ⊕ 1 ⊕ 1 ⊕ 1 = " ++ show (head (c2_eval [(C2 1), (C2 1), (C2 1), (C2 1)]))
+  putStrLn $ "1 ⊕ 1 ⊕ 1 = " ++ show (mconcat [C2 1, C2 1, C2 1])
+  putStrLn $ "1 ⊕ 1 ⊕ 1 ⊕ 1 = " ++ show (mconcat [C2 1, C2 1, C2 1, C2 1])
