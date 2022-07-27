345e39b74b940e7a6a39d2142276ab0370e6b31a
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-27 22:03:20 +0900

    refactor(functions/add_2arg_num): Improve example of Rational.

--- src/functions/add_2arg_num.hs
+++ src/functions/add_2arg_num.hs
@@ -5,10 +5,10 @@ main :: IO ()
 main = do
     print $ add (42 :: Int) 13      == 55
     print $ add (42 :: Integer) 13  == 55
-    print $ add (42 :: Rational) 13 == 55
+    print $ ((==) (5/6 :: Rational)) (add (1/3) (1/2) :: Rational)
     print $ add (42 :: Float) 13    == 55
     print $ add (42 :: Double) 13   == 55
-    -- NG: Int type overflow 
+    -- NG: Int type overflow
     print $
       add (maxBound :: Int) 1             == (minBound :: Int)
     -- OK: Integer type
