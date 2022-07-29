7a92dd1df11d74d0d683ade3bac86e5bda1b3fdc
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-29 21:17:51 +0900

    feat: Upgrade Haskell 8.0.2 to 8.4.4

---
+++ src/math/group/group_o2_c2_using_monoid_before_GHC_8.4.hs
@@ -0,0 +1,24 @@
+data C2 = C2 Int
+
+instance Eq C2 where
+  (C2 x) == (C2 x') = x == x'
+
+instance Show C2 where
+  show a = case a of
+    C2 0 -> "0"
+    C2 1 -> "1"
+    _    -> "?"
+
+instance Monoid C2 where
+  mempty = C2 0
+  mappend (C2 x) (C2 y) = C2 ((x + y) `mod` 2)
+
+c2_inv :: C2 -> C2
+c2_inv (C2 x) = head [C2 y | y <- [0..2], (C2 x) `mappend` (C2 y) == (mempty)]
+
+main :: IO ()
+main = do
+  putStrLn $ "0^(-1) = " ++ show (c2_inv (C2 0))
+  putStrLn $ "1^(-1) = " ++ show (c2_inv (C2 1))
+  putStrLn $ "1 ⊕ 1 ⊕ 1 = " ++ show (mconcat [C2 1, C2 1, C2 1])
+  putStrLn $ "1 ⊕ 1 ⊕ 1 ⊕ 1 = " ++ show (mconcat [C2 1, C2 1, C2 1, C2 1])
