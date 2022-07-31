6999f533186a834132a947c00030b521adbad438
unmyr <56982541+unmyr@users.noreply.github.com>
2022-07-31 20:48:49 +0900

    fix: HN to NH.

--- src/math/group/group_o6_d3_sd.hs
+++ src/math/group/group_o6_d3_sd.hs
@@ -9,12 +9,12 @@ d3_sym "e"   = C3xdC2 0 0
 d3_sym "r"   = C3xdC2 1 0
 d3_sym "rr"  = C3xdC2 2 0
 d3_sym "s"   = C3xdC2 0 1
-d3_sym "sr"  = C3xdC2 2 1
-d3_sym "srr" = C3xdC2 1 1
+d3_sym "rs"  = C3xdC2 1 1
+d3_sym "rrs" = C3xdC2 2 1
 d3_sym  _  = error "ERROR: Invalid argument."
 
 d3_all :: [D3]
-d3_all = [d3_sym "e", d3_sym "r", d3_sym "rr", d3_sym "s", d3_sym "sr", d3_sym "srr"]
+d3_all = [d3_sym "e", d3_sym "r", d3_sym "rr", d3_sym "s", d3_sym "rs", d3_sym "rrs"]
 
 instance Show D3 where
   show a = case a of
@@ -22,8 +22,8 @@ instance Show D3 where
     C3xdC2 1 0 -> "r"
     C3xdC2 2 0 -> "rr"
     C3xdC2 0 1 -> "s"
-    C3xdC2 1 1 -> "srr"
-    C3xdC2 2 1 -> "sr"
+    C3xdC2 1 1 -> "rs"
+    C3xdC2 2 1 -> "rrs"
     C3xdC2 r s -> "(" ++ show r ++ "," ++ show s ++ ")"
 
 phi :: Int -> Int -> Int
@@ -43,13 +43,13 @@ d3_inv (C3xdC2 r_x s_x) = head [C3xdC2 r_y s_y | r_y <- [0, 1, 2], s_y <- [0, 1]
 
 main :: IO ()
 main = do
-  print $ [(d3_sym "e")   <> y | y <- d3_all] == [  d3_sym "e",   d3_sym "r",  d3_sym "rr",   d3_sym "s",  d3_sym "sr", d3_sym "srr"]
-  print $ [(d3_sym "r")   <> y | y <- d3_all] == [  d3_sym "r",  d3_sym "rr",   d3_sym "e", d3_sym "srr",   d3_sym "s",  d3_sym "sr"]
-  print $ [(d3_sym "rr")  <> y | y <- d3_all] == [ d3_sym "rr",   d3_sym "e",   d3_sym "r",  d3_sym "sr", d3_sym "srr",   d3_sym "s"]
-  print $ [(d3_sym "s")   <> y | y <- d3_all] == [  d3_sym "s",  d3_sym "sr", d3_sym "srr",   d3_sym "e",   d3_sym "r",  d3_sym "rr"]
-  print $ [(d3_sym "sr")  <> y | y <- d3_all] == [ d3_sym "sr", d3_sym "srr",   d3_sym "s",  d3_sym "rr",   d3_sym "e",   d3_sym "r"]
-  print $ [(d3_sym "srr") <> y | y <- d3_all] == [d3_sym "srr",   d3_sym "s",  d3_sym "sr",   d3_sym "r",  d3_sym "rr",   d3_sym "e"]
+  print $ [(d3_sym "e")   <> y | y <- d3_all] == [  d3_sym "e",   d3_sym "r",  d3_sym "rr",   d3_sym "s",  d3_sym "rs", d3_sym "rrs"]
+  print $ [(d3_sym "r")   <> y | y <- d3_all] == [  d3_sym "r",  d3_sym "rr",   d3_sym "e",  d3_sym "rs", d3_sym "rrs",   d3_sym "s"]
+  print $ [(d3_sym "rr")  <> y | y <- d3_all] == [ d3_sym "rr",   d3_sym "e",   d3_sym "r", d3_sym "rrs",   d3_sym "s",  d3_sym "rs"]
+  print $ [(d3_sym "s")   <> y | y <- d3_all] == [  d3_sym "s", d3_sym "rrs",  d3_sym "rs",   d3_sym "e",  d3_sym "rr",   d3_sym "r"]
+  print $ [(d3_sym "rs")  <> y | y <- d3_all] == [ d3_sym "rs",   d3_sym "s", d3_sym "rrs",   d3_sym "r",   d3_sym "e",  d3_sym "rr"]
+  print $ [(d3_sym "rrs") <> y | y <- d3_all] == [ d3_sym "rrs",  d3_sym "rs",  d3_sym "s",  d3_sym "rr",   d3_sym "r",   d3_sym "e"]
   print $ mconcat [d3_sym "s", d3_sym "r", d3_sym "s"] == d3_sym "rr"
   putStrLn $ "s * r * s = " ++ show (mconcat [d3_sym "s", d3_sym "r", d3_sym "s"]) -- == d3_sym "rr"
-  print $ d3_inv (d3_sym "sr") == d3_sym "sr"
-  putStrLn $ "(sr)^(-1) = " ++ show (d3_inv (d3_sym "sr")) -- == d3_sym "sr"
+  print $ d3_inv (d3_sym "rrs") == d3_sym "rrs"
+  putStrLn $ "(rrs)^(-1) = " ++ show (d3_inv (d3_sym "rrs")) -- == d3_sym "rrs"
