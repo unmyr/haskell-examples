94931441d0628c98f00b12df06f3d010bafc35ac
unmyr <56982541+unmyr@users.noreply.github.com>
2022-08-06 21:09:26 +0900

    feat(square_of_list): Add example of Functor, Applicative and Monad.

--- src/data-types/list/square_of_list.hs
+++ src/data-types/list/square_of_list.hs
@@ -4,6 +4,11 @@ sqList :: (Integral t) => [t] -> [t]
 sqList [] = []
 sqList (x:xs) = (x * x):sqList(xs)
 
+sqListMonad :: Int -> [Int]
+sqListMonad n = do
+  value <- [1 .. n]
+  return (value^(2::Int))
+
 traceMyFoldrLH :: (Show a, Show b) => (a -> b -> b) -> b -> [a] -> b
 traceMyFoldrLH _ z [] =
   trace("traceMyFoldrLH f " ++ (show z) ++ " [] = " ++ (show z))
@@ -22,24 +27,55 @@ traceMyFoldlLH f z (x:xs) =
 
 main :: IO ()
 main = do
-  putStrLn "-- take 5 $ map (\\x -> x * x) ([1..] :: [Int])"
-  print $ take 5 $ map (\x -> x * x) ([1..] :: [Int])
-  putStrLn "-- take 5 $ map (^(2::Int)) ([1..] :: [Int])"
-  print $ take 5 $ map (^(2::Int)) ([1..] :: [Int])
-  putStrLn "-- take 5 $ fmap (\\x -> x * x) ([1..] :: [Int])"
-  print $ take 5 $ fmap (\x -> x * x) ([1..] :: [Int])
-  putStrLn "-- take 5 $ foldr (\\x xs -> (x * x) : xs) [] ([1..] :: [Int])"
-  print $ take 5 $ foldr (\x xs -> (x * x) : xs) [] ([1..] :: [Int])
-  putStrLn "-- take 5 $ [x * x | x <- ([1..] :: [Int])]"
-  print $ take 5 $ [x * x | x <- ([1..] :: [Int])]
-  putStrLn "-- take 5 $ ([1..] :: [Int]) >>= \\x -> pure (x * x)"
-  print $ take 5 $ ([1..] :: [Int]) >>= \x -> pure (x * x)
+  -- Using recursive function
   putStrLn "-- take 5 $ sqList ([1..] :: [Int])"
   print $ take 5 $ sqList ([1..] :: [Int])
 
-  putStrLn "-- traceMyFoldrLH (\\x y -> x) 0 ([1..] :: [Int])"
+  -- Using `map`
+  putStrLn "\n-- take 5 $ map (\\x -> x * x) ([1..] :: [Int])"
+  print $ take 5 $ map (\x -> x * x) ([1..] :: [Int])
+  putStrLn "\n-- take 5 $ map (^(2::Int)) ([1..] :: [Int])"
+  print $ take 5 $ map (^(2::Int)) ([1..] :: [Int])
+
+  -- Using `fmap`
+  putStrLn "\n-- take 5 $ fmap (\\x -> x * x) ([1..] :: [Int])"
+  print $ take 5 $ fmap (\x -> x * x) ([1..] :: [Int])
+  -- using an infix synonym(<$>) for `Data.Functor.fmap`.
+  putStrLn "\n-- take 5 $ (\\x -> x * x) <$> ([1..] :: [Int])"
+  print $ take 5 $ (\x -> x * x) <$> ([1..] :: [Int])
+
+  -- using list comprehension
+  putStrLn "\n-- take 5 $ [x * x | x <- ([1..] :: [Int])]"
+  print $ take 5 $ [x * x | x <- ([1..] :: [Int])]
+
+  -- Using `foldr`
+  putStrLn "\n-- take 5 $ foldr (\\x xs -> (x * x) : xs) [] ([1..] :: [Int])"
+  print $ take 5 $ foldr (\x xs -> (x * x) : xs) [] ([1..] :: [Int])
+
+  -- Applicative 1
+  putStrLn "\n-- take 5 $ pure (^(2::Int)) <*> ([1..] :: [Int])"
+  print $ take 5 $ pure (^(2::Int)) <*> ([1..] :: [Int])
+  -- Applicative 2
+  putStrLn "\n-- take 5 $ [(^(2::Int))] <*> ([1..] :: [Int])"
+  print $ take 5 $ [(^(2::Int))] <*> ([1..] :: [Int])
+
+  -- With Monad 1
+  putStrLn "\n-- take 5 $ ([1..] :: [Int]) >>= \\x -> pure (x * x)"
+  print $ take 5 $ ([1..] :: [Int]) >>= \x -> pure (x * x)
+  -- With Monad 2
+  putStrLn "\n-- take 5 $ do {value <- ([1..] :: [Int]); pure (value ^ (2::Int))}"
+  print $ take 5 $ do {value <- ([1..] :: [Int]); pure (value ^ (2::Int))}
+  -- With Monad 3
+  putStrLn "\n-- sqListMonad 5"
+  print $ sqListMonad 5
+  -- With Monad 4
+  putStrLn "\n-- take 5 $ (\\n -> pure (n * n)) =<< ([1..] :: [Int])"
+  print $ take 5 $ (\n -> pure (n * n)) =<< ([1..] :: [Int])
+
+  -- foldr
+  putStrLn "\n-- traceMyFoldrLH (\\x y -> x) 0 ([1..] :: [Int])"
   print $ traceMyFoldrLH (\x _ -> trace("(\\x y -> x) = " ++ (show x)) $ x) (0 :: Int) ([1..] :: [Int])
-  putStrLn "-- take 5 $ traceMyFoldrLH (\\x y -> trace(\"(\\x y -> (x * x) : y) = \" ++ (show (x * x)) ++ \":\" ++ show y) $ (x * x) : y) [] ([1..6] :: [Int])"
+  putStrLn "\n-- take 5 $ traceMyFoldrLH (\\x y -> trace(\"(\\x y -> (x * x) : y) = \" ++ (show (x * x)) ++ \":\" ++ show y) $ (x * x) : y) [] ([1..6] :: [Int])"
   print $ take 5 $ traceMyFoldrLH (\x y -> trace("(\\x y -> (x * x) : y) = " ++ (show (x * x)) ++ ":" ++ show y) $ (x * x) : y) [] ([1..6] :: [Int])
-  putStrLn "-- take 5 $ traceMyFoldrLH (\\x y -> trace(\"(\\x y -> (x * x) : y) = \" ++ (show (x * x)) ++ \":y\") $ (x * x) : y) [] ([1..] :: [Int])"
+  putStrLn "\n-- take 5 $ traceMyFoldrLH (\\x y -> trace(\"(\\x y -> (x * x) : y) = \" ++ (show (x * x)) ++ \":y\") $ (x * x) : y) [] ([1..] :: [Int])"
   print $ take 5 $ traceMyFoldrLH (\x y -> trace("(\\x y -> (x * x) : y) = " ++ (show (x * x)) ++ ":y") $ (x * x) : y) [] ([1..] :: [Int])
