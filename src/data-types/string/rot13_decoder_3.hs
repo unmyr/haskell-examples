4e9a122546c8d18c46ede6615559d02c81a6c7c9
unmyr <56982541+unmyr@users.noreply.github.com>
2022-08-09 19:50:08 +0900

    refactor(data-types/string): Refactor a code.

--- src/data-types/string/rot13_decoder_3.hs
+++ src/data-types/string/rot13_decoder_3.hs
@@ -1,14 +1,15 @@
+import Data.Char (isLower, isUpper)
+
 decodeRot13 :: String -> String
 decodeRot13 s = map rot13Char s
   where
-    -- [('a','n'),('b','o'), ...,('z','m')]
-    lowerMap = zip ['a'..'z'] (take (length ['a'..'z']) (drop 13 (cycle ['a'..'z'])))
-    -- [('A','N'),('B','O'), ...,('Z','M')]
-    upperMap = zip ['A'..'Z'] (take (length ['A'..'Z']) (drop 13 (cycle ['A'..'Z'])))
-    alphaMap = lowerMap ++ upperMap
-    rot13Char c = case lookup c alphaMap of
-        Just value -> value
-        Nothing -> c
+    makePairsRot13 cl = zip cl ((drop 13 cl) ++ (take 13 cl))
+    lowerPairs = makePairsRot13 ['a'..'z'] -- [('a','n'),('b','o'), ...,('z','m')]
+    upperPairs = makePairsRot13 ['A'..'Z'] -- [('A','N'),('B','O'), ...,('Z','M')]
+    rot13Char c = case c of
+      ch | isLower ch -> snd $ lowerPairs !! (fromEnum ch - fromEnum 'a')
+      ch | isUpper ch -> snd $ upperPairs !! (fromEnum ch - fromEnum 'A')
+      ch -> ch
 
 main :: IO ()
 main = do
