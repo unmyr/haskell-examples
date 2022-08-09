918580062426ee9819bb0b1385da51e759b37ff6
unmyr <56982541+unmyr@users.noreply.github.com>
2022-08-09 20:02:56 +0900

    refactor(data-types/string): Refactor a code.

--- src/data-types/string/rot13_decoder_3.hs
+++ src/data-types/string/rot13_decoder_3.hs
@@ -6,10 +6,9 @@ decodeRot13 s = map rot13Char s
     makePairsRot13 cl = zip cl ((drop 13 cl) ++ (take 13 cl))
     lowerPairs = makePairsRot13 ['a'..'z'] -- [('a','n'),('b','o'), ...,('z','m')]
     upperPairs = makePairsRot13 ['A'..'Z'] -- [('A','N'),('B','O'), ...,('Z','M')]
-    rot13Char c = case c of
-      ch | isLower ch -> snd $ lowerPairs !! (fromEnum ch - fromEnum 'a')
-      ch | isUpper ch -> snd $ upperPairs !! (fromEnum ch - fromEnum 'A')
-      ch -> ch
+    rot13Char c | isLower c = snd $ lowerPairs !! (fromEnum c - fromEnum 'a')
+    rot13Char c | isUpper c = snd $ upperPairs !! (fromEnum c - fromEnum 'A')
+    rot13Char c = c
 
 main :: IO ()
 main = do
