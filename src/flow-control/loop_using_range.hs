188965a561e2c7bb7d7fd9241c33d7462ef5dd65
unmyr <56982541+unmyr@users.noreply.github.com>
2022-08-10 19:30:55 +0900

    refactor(flow-control): Refactor codes.

--- src/flow-control/loop_using_range.hs
+++ src/flow-control/loop_using_range.hs
@@ -1,9 +1,9 @@
 main :: IO ()
 main = do
-  let loop i n = case (i < n) of {
+  let loop (i, n) = case (i < n) of {
     False -> return ();
     True -> do
       print i
-      loop (i + 1) n
+      loop (i + 1,  n)
   }
-  loop (1 :: Int) 6
+  loop (1 :: Int, 6)
